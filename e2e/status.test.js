f2d0e0a123c902a86d0ce85dabdd8cdbf3106ffc
Dmitry Sichkar <dmmeteo@gmail.com>
2018-04-04 09:25:18 +0300

    fix status e2e

--- e2e/status.test.js
+++ e2e/status.test.js
@@ -17,3 +17,30 @@ test(`should display the page if user is not logged in`, async(t) => {
         .expect(Selector('a').withText('Register').exists).ok()
         .expect(Selector('a').withText('Log In').exists).ok()
 })
+
+test(`should display user info if user is logged in`, async(t) => {
+    // register user
+    await t
+        .navigateTo(`${TEST_URL}/register`)
+        .typeText('input[name="username"]', username)
+        .typeText('input[name="email"]', email)
+        .typeText('input[name="password"]', 'test')
+        .click(Selector('input[type="submit"]'))
+
+    // assert '/status' is displayed property
+    await t
+        .navigateTo(`${TEST_URL}/status`)
+        .expect(Selector('li > strong').withText('User ID:').exists).ok()
+        .expect(Selector('li > strong').withText('Email:').exists).ok()
+        .expect(Selector('li').withText(email).exists).ok()
+        .expect(Selector('li > strong').withText('Username:').exists).ok()
+        .expect(Selector('li').withText(username).exists).ok()
+        .expect(Selector('a').withText('User Status').exists).ok()
+        .expect(Selector('a').withText('Log Out').exists).ok()
+        .expect(Selector('a').withText('Register').exists).notOk()
+        .expect(Selector('a').withText('Log In').exists).notOk()
+})
+
+
+
+
