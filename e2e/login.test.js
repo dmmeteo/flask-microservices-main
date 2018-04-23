ebed8a9e0bd0af199731acdfe0c41de83c2f2d99
Dmitry Sichkar <dmmeteo@gmail.com>
2018-04-23 09:52:49 +0300

    test login message

--- e2e/login.test.js
+++ e2e/login.test.js
@@ -119,4 +119,17 @@ test(`should validate the password field`, async(t) => {
             .withText('Password must be greater than 10 characters.').exists).ok()
 })
 
+test(`should display the page correctly if a user is not logged in`, async(t) => {
+    await t
+        .navigateTo(TEST_URL)
+        .expect(Selector('H1').withText('All Users').exists).ok()
+        .expect(Selector('a').withText('User Status').exists).notOk()
+        .expect(Selector('a').withText('Log Out').exists).notOk()
+        .expect(Selector('a').withText('Register').exists).ok()
+        .expect(Selector('a').withText('Log In').exists).ok()
+        .expect(Selector('.alert').exists).notOk()
+})
+
+
+
 
